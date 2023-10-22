2ec99a9358e7e7c1e87f21ece378e0fde97fd599
BaBa7080 <abhishekbaba7080@gmail.com>
2023-10-23 02:07:43 +0530

    redirect uri2

--- connector/send.py
+++ connector/send.py
@@ -7,6 +7,7 @@ from django.shortcuts import render, redirect
 import requests
 from django.http import HttpResponse
 from urllib.parse import unquote
+from django.views.decorators.csrf import csrf_exempt
 class YourAPIView(APIView):
     def get(self, request):
         queryset = Products.objects.all()
@@ -27,38 +28,54 @@ def facebookAouthredirect(req):
     savedata = facebookresponce.objects.create(data=data,typee="redirect")
     return True
     # response = redirect(url)
-
-def facebookAouth(req):
-    # print(req)
-    # appid = 253343950955737
-    # url = "https://www.facebook.com/v17.0/dialog/oauth?client_id="+appid+"&redirect_uri={"https://www.domain.com/login"}&state={"{st=state123abc,ds=123456789}"}
-
+from urllib.parse import urlencode
+def facebookAouth(request):
     base_url = "https://www.facebook.com/v17.0/dialog/oauth"
     client_id = "253343950955737"
     redirect_uri = "https://shopbyte-93c07294e0e6.herokuapp.com/connector/facebokredirect/"
-    state = '{"st": "state123abc", "ds": "123456789"}'
+    state = "state123abc"
 
     params = {
         "client_id": client_id,
         "redirect_uri": redirect_uri,
-        "state": state
+        "state": state,
     }
 
-    url = f"{base_url}?{requests.compat.urlencode(params)}"
-    print(url)
-    response = requests.get(url)
+    url = f"{base_url}?{urlencode(params)}"
 
-    if response.status_code == 200:
-        print("Request successful!")
-        print("Response content:")
-        print(response.text)
-        # rew = requests.get(response)
-        decoded_content = unquote(response.text)
-        return HttpResponse(decoded_content, content_type='text/html')
-    else:
-        print(f"Request failed with status code: {response.status_code}")
+    return redirect(url)
+# @csrf_exempt
+# def facebookAouth(req):
+#     # print(req)
+#     # appid = 253343950955737
+#     # url = "https://www.facebook.com/v17.0/dialog/oauth?client_id="+appid+"&redirect_uri={"https://www.domain.com/login"}&state={"{st=state123abc,ds=123456789}"}
 
-    response = redirect(url)
-    print(response)
-    return HttpResponse(f"Request failed with status code: {response.status_code}", status=500)
+#     base_url = "https://www.facebook.com/v17.0/dialog/oauth"
+#     client_id = "253343950955737"
+#     redirect_uri = "https://shopbyte-93c07294e0e6.herokuapp.com/connector/facebokredirect/"
+#     state = '{"st": "state123abc", "ds": "123456789"}'
+
+#     params = {
+#         "client_id": client_id,
+#         "redirect_uri": redirect_uri,
+#         "state": state
+#     }
+
+#     url = f"{base_url}?{requests.compat.urlencode(params)}"
+#     print(url)
+#     response = requests.get(url)
+
+#     if response.status_code == 200:
+#         print("Request successful!")
+#         print("Response content:")
+#         print(response.text)
+#         # rew = requests.get(response)
+#         decoded_content = unquote(response.text)
+#         return HttpResponse(decoded_content, content_type='text/html')
+#     else:
+#         print(f"Request failed with status code: {response.status_code}")
+
+#     response = redirect(url)
+#     print(response)
+#     return HttpResponse(f"Request failed with status code: {response.status_code}", status=500)
 
